ea544ca2b02f6065506871d1a47298a727f56bc4
sburakbahceci <safaburakbahceci29@gmail.com>
2020-09-30 15:18:18 +0300

    Finish Structure and Styles

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -9,9 +9,9 @@ class TodoItemsController < ApplicationController
     def destroy
         @todo_item = @todo_list.todo_items.find(params[:id])
         if @todo_item.destroy
-            flash[:success] = "Todo List item was deleted."
+            flash[:success] = "Yapılacaklar listesi silindi."
         else
-            flash[:error] = "Todo List item could not be deleted."
+            flash[:error] = "Yapılacaklar listesi silinemedi."
             
         end
         redirect_to @todo_list
