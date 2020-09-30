240c799c496d9226eb9b104c8bad0d914264d09a
sburakbahceci <safaburakbahceci29@gmail.com>
2020-09-30 14:11:12 +0300

    Mark as completed Ability

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,5 +1,6 @@
 class TodoItemsController < ApplicationController
     before_action :set_todo_list
+    before_action :set_todo_item, except: [:create]
     def create
         @todo_item = @todo_list.todo_items.create(todo_item_params)
         redirect_to @todo_list
@@ -14,6 +15,14 @@ class TodoItemsController < ApplicationController
             
         end
         redirect_to @todo_list
+    end
+    def complete
+        @todo_item.update_attribute(:completed_at,Time.now)
+        redirect_to @todo_list, notice: "Tamamlandı."
+    end
+
+    def set_todo_item
+        @todo_item = @todo_list.todo_items.find(params[:id])
     end
 
     private
