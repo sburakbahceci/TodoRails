afdc93d2935903296e7e87d38dda9e15802e93cf
sburakbahceci <safaburakbahceci29@gmail.com>
2020-09-30 13:41:17 +0300

    Added TodoList & TodoItem

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :todo_lists do
+    resources :todo_items
+  end
+  root "todo_lists#index"
+
 end
